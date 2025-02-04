516dabeb63779778895d3687369fb8dd7f405837
Angelo Salles <angelo.salles@mercadolivre.com>
2025-02-04 13:25:13 -0300

    removing custom generator

--- src/main/java/com/todo/todolist/domain/PrimaryIDGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.todo.todolist.domain;
-
-import org.hibernate.annotations.IdGeneratorType;
-import org.hibernate.id.IncrementGenerator;
-
-@IdGeneratorType(IncrementGenerator.class)
-public @interface PrimaryIDGenerator {
-}
